b51d5f8318a91bc219744da8baeb4dfafba9017e
Ankitha2807 <knankitha73@gmail.com>
2026-03-05 23:25:30 +0530

    war deployment version

--- src/main/java/com/example/demo/Sample.java
+++ src/main/java/com/example/demo/Sample.java
@@ -1,10 +1,20 @@
 package com.example.demo;
+
 import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.bind.annotation.GetMapping;
+
 @RestController
-public class Sample {
+
+public class Sample 
+
+{
+	
 @GetMapping("/display")
-public String display() {
-return "welcome to my first spring boot aws application";
+	public String display() 
+	{
+		return "welcome to my first spring boot aws application";
+	}
+
 }
-}
+
