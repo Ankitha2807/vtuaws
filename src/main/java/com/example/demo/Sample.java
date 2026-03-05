823363624781565d03691ce5975bab6bacc7eb0d
Ankitha2807 <knankitha73@gmail.com>
2026-03-05 12:47:23 +0530

    first commit

---
+++ src/main/java/com/example/demo/Sample.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+@RestController
+public class Sample {
+@GetMapping("/display")
+public String display() {
+return "welcome to my first spring boot aws application";
+}
+}
---
+++ src/main/java/com/example/demo/VtuawsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VtuawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VtuawsApplication.class, args);
+	}
+
+}
